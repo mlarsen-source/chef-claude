7a9ea46af604f9a829f7ee280fa0a39a30ba29e6
mlarsen <174657206+mlarsen-source@users.noreply.github.com>
2025-10-08 20:26:17 -0700

    modified

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,32 +1,29 @@
 export default function Main() {
   
-  const ingredients = ["Chicken", "Oregano", "Tomatoes"]
+  const ingredients = ["Chicken", "Oregano", "Tomatoes"];
 
-  const ingredientsListItems = ingredients.map(ingredient => (
+  const ingredientsListItems = ingredients.map((ingredient) => (
     <li key={ingredient}>{ingredient}</li>
-  ))
+  ));
 
   function handleSubmit(event) {
-    
-    event.preventDefault()
-    const formData = new FormData(event.currentTarget)
-    const newIngredient = formData.get("ingredient")
-
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const newIngredient = formData.get("ingredient");
   }
-    
+
   return (
     <main>
-      <form onSubmit={handleSubmit} className="ingredient-entry-form">
+      <form
+        onSubmit={handleSubmit}
+        className="ingredient-entry-form">
         <input
           type="text"
           placeholder="e.g. chicken"
-          aria-label="add ingredient">
-        </input>
+          aria-label="add ingredient"></input>
         <button>+ Add ingredient</button>
       </form>
-      <ul>
-        {ingredientsListItems}
-      </ul>
+      <ul>{ingredientsListItems}</ul>
     </main>
-  )
+  );
 }
