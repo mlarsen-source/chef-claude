bf3e25d34d1e60c2d3ec1c936034d2d50f2a7696
mlarsen <174657206+mlarsen-source@users.noreply.github.com>
2025-10-12 14:48:24 -0700

    refactored code

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import ClaudeRecipe from './ClaudeRecipe'
+import IngredientsList from './IngredientsList'
 
 export default function Main() {
   
   const [ingredients, setIngredients] = React.useState([]);
+  const [recipeShown, setRecipeShown] = React.useState(false)
 
-  const ingredientsListItems = ingredients.map((ingredient) => (
-    <li key={ingredient}>{ingredient}</li>
-  ));
-
+  function toggleRecipeShown() {
+    setRecipeShown(prevShown => !prevShown)
+  }
+  
   function addIngredient(formData) {
     const newIngredient = formData.get("ingredient");
     setIngredients(prevIngredients=>([...prevIngredients, newIngredient]))
@@ -26,19 +29,12 @@ export default function Main() {
         </input>
         <button>+ Add ingredient</button>
       </form>
-      {ingredients.length > 0 &&
-      <section>
-        <h2>Ingredients on hand:</h2>
-        <ul className="ingredients-list">{ingredientsListItems}</ul>
-        {ingredients.length > 3 &&
-          <div className="get-recipe-container">
-            <div>
-              <h3>Ready for a recipe?</h3>
-              <p>Generate a recipe from your list of ingredients.</p>
-              <button>Get a recipe</button>
-            </div>
-          </div>}
-      </section>}
+      {ingredients.length > 0 && 
+        <IngredientsList
+          ingredients={ingredients}
+          toggle = {toggleRecipeShown}
+        />}      
+      {recipeShown && <ClaudeRecipe/>}
     </main>
   );
 }
